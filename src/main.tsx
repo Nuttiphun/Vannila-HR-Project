2c2f37c59de53ee29ff7df5e4a5213627dc53718
Nuttiphun <nutchanock12@gmail.com>
2024-10-03 20:44:52 +0700

    setup tailwind css
    - install package
    - fix some format with prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
+import "./main.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-  
-  </StrictMode>,
-)
+  </StrictMode>
+);
